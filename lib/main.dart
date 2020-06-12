5264949028fe9b509aaba17231810d8b98735538
Hush <rajputsiddharth18@gmail.com>
2020-06-13 00:06:49 +0530

    added button questions changing on press

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,46 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main()
 {
   runApp(MyApp()); 
 }
 
-class MyApp extends StatelessWidget {
-  void answerQuestion()
-  {
+class MyApp extends StatefulWidget {
+  
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionindex  = 0;
+
+  void _answerQuestion()
+  { 
+    setState(() {
+      _questionindex = _questionindex + 1;
+    }); 
+    print(_questionindex);
     print("Answer Chosen!");
   }
+
   @override 
   Widget build(BuildContext context) {
     var questions = ["What is Your Favourite Color?","What is your Favourite Animal"];
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title:Text("Quiz App")
         ),
         body: Column(
           children: <Widget>[
-            Text("This is a Question!"),
-            RaisedButton(onPressed: answerQuestion ,child: Text("Answer1"),),
-            RaisedButton(onPressed: answerQuestion,child: Text("Answer2"),),
-            RaisedButton(onPressed: answerQuestion,child: Text("Answer3"),),
-            RaisedButton(onPressed: answerQuestion,child: Text("Answer4"),),
+            Question(questions[_questionindex]),
+            RaisedButton(onPressed: _answerQuestion ,child: Text("Answer1"),),
+            RaisedButton(onPressed: _answerQuestion,child: Text("Answer2"),),
+            RaisedButton(onPressed: _answerQuestion,child: Text("Answer3"),),
+            RaisedButton(onPressed: _answerQuestion,child: Text("Answer4"),),
           ],
         ),
       ),
